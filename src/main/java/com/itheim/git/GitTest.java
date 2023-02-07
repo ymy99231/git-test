13109e01f4ae66c0cd1f2091847633f759761a0f
YangYi <1192299231@qq.com>
2023-02-07 14:41:58 +0800

    yy commit

--- src/main/java/com/itheim/git/GitTest.java
+++ src/main/java/com/itheim/git/GitTest.java
@@ -3,5 +3,6 @@ package com.itheim.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("你好呀！");
     }
 }
