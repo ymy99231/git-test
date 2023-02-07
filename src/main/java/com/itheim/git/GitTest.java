53b775feee2789827bbadcf4d270fa4f41d035ad
ymy99231 <124649283+ymy99231@users.noreply.github.com>
2023-02-07 15:39:39 +0800

    pull commit

--- src/main/java/com/itheim/git/GitTest.java
+++ src/main/java/com/itheim/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git master test");
         System.out.println("hello git test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
