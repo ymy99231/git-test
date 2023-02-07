f903c1e11e16cce9fa719bfea7143408984ef7ad
YangYi <1192299231@qq.com>
2023-02-07 14:56:46 +0800

    yy hot-fix commit1

--- src/main/java/com/itheim/git/GitTest.java
+++ src/main/java/com/itheim/git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("你好呀！");
-        System.out.println("你好呀2！");
+        System.out.println("hello git4");
     }
 }
